f57a0dd2f7cb1be495cbab8a6fb31a3d6331ff98
Zikora <zikorapascal@gmail.com>
2025-11-15 16:52:15 +0000

    deplyment changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
+import { devtools } from '@vue/devtools'
 import App from './App.vue'
 import './assets/main.css'
 import 'primeicons/primeicons.css'
+
+if (process.env.NODE_ENV === 'development') {
+  import('@vue/devtools').then((devtools) => {
+    devtools.connect()
+  })
+}
 
 const app = createApp(App)
 
